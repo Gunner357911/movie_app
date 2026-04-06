eebe37dde06558ae42ff5e380e222b578e0d0f75
Gunner357911 <gunyuritv7@gmail.com>
2026-04-06 19:18:28 +0700

    feat: add api.py

--- api.py
+++ api.py
@@ -1,5 +1,24 @@
 import psycopg2
 from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+import pandas as pd
+
+load_dotenv()
 
 app = FastAPI()
 
+def get_conn():
+    db_url = os.getenv("SUPABASE_DB_URL") 
+# or st.secrets["SUPABASE_DB_URL"]
+    conn = psycopg2.connect(db_url)
+
+    return conn
+
+@app.get("/show")
+def get_all_data():
+    conn = get_conn()
+
+    df = pd.read_sql_query("SELECT * FROM movie_rating", conn)
+
+    return df
